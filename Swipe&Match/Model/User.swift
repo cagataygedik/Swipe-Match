59f660ce01ae4cfae3e1df0c3ca41c71c914e7b5
cagatay <cagatay.gedik@icloud.com>
2023-03-27 18:45:20 +0300

    Created user model.

---
+++ Swipe&Match/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Swipe&Match
+//
+//  Created by Celil Çağatay Gedik on 27.03.2023.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let age: Int
+    let profession: String
+    let imageName: String
+}
