b3c9bb48af8351a76799b56d2dcda07e4903abc3
cagatay <cagatay.gedik@icloud.com>
2023-03-28 11:14:09 +0300

    Created CardViewModel.

---
+++ Swipe&Match/ViewModels/CardViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CardViewModel.swift
+//  Swipe&Match
+//
+//  Created by Celil Çağatay Gedik on 28.03.2023.
+//
+
+import UIKit
+
+struct CardViewModel {
+    let imageName: String
+    let attributedString: NSMutableAttributedString
+    let textAlignment: NSTextAlignment
+}
