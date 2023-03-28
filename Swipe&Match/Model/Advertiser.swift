16a0351c3253364f9b0f7983cac19a6af7cb6612
cagatay <cagatay.gedik@icloud.com>
2023-03-28 16:08:13 +0300

    Created new Advertiser Model.

---
+++ Swipe&Match/Model/Advertiser.swift
@@ -0,0 +1,20 @@
+//
+//  Advertisor.swift
+//  Swipe&Match
+//
+//  Created by Celil Çağatay Gedik on 28.03.2023.
+//
+
+import UIKit
+
+struct Advertiser: ProducesCardViewModel {
+    let title: String
+    let brandName: String
+    let posterPhotoName: String
+    
+    func toCardViewModel() -> CardViewModel {
+        let attributedString = NSMutableAttributedString(string: title, attributes: [.font: UIFont.systemFont(ofSize: 34, weight: .heavy)])
+        attributedString.append(NSAttributedString(string: "\n" + brandName, attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .bold)]))
+        return CardViewModel(imageName: posterPhotoName, attributedString: attributedString, textAlignment: .center)
+    }
+}
