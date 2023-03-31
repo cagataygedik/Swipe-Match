0dff14d7fc6b2252b3e41d213cdee1bc236ce4cb
cagatay <cagatay.gedik@icloud.com>
2023-03-31 18:48:22 +0300

    Created new constants.

--- Swipe&Match/Utilities/Constants.swift
+++ Swipe&Match/Utilities/Constants.swift
@@ -21,3 +21,20 @@ enum BottomImages {
     static let like = UIImage(named: "like_circle")
     static let boost = UIImage(named: "boost_circle")
 }
+
+enum Placeholder {
+    static let fullName = "Enter full name"
+    static let email = "Enter e-mail"
+    static let password = "Enter password"
+}
+
+enum Color {
+    static let top = UIColor(red: 0.9921568627, green: 0.3568627451, blue: 0.3725490196, alpha: 1.0)
+    static let bottom = UIColor(red: 0.8980392157, green: 0.0, blue: 0.4470588235, alpha: 1.0)
+    static let button = UIColor(red: 0.8235294118, green: 0.0, blue: 0.3254901961, alpha: 1.0)
+}
+
+enum Title {
+    static let photo = "Select a photo"
+    static let register = "Register"
+}
