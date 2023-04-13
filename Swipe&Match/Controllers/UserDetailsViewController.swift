3445b66aa0f406d3ec40d6c66ef97999d8e4679f
cagatay <cagatay.gedik@icloud.com>
2023-04-13 20:56:36 +0300

    Added new buttons to the view contoller.

--- Swipe&Match/Controllers/UserDetailsViewController.swift
+++ Swipe&Match/Controllers/UserDetailsViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import SDWebImage
 
 class UserDetailsViewController: UIViewController, UIScrollViewDelegate {
     
+    var cardViewModel: CardViewModel! {
+        didSet {
+            infoLabel.attributedText = cardViewModel.attributedString
+            
+            guard let firstImageUrl = cardViewModel.imageNames.first, let url = URL(string: firstImageUrl) else { return }
+            imageView.sd_setImage(with: url)
+        }
+    }
+    
     lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.alwaysBounceVertical = true
         scrollView.contentInsetAdjustmentBehavior = .never
         scrollView.delegate = self
@@ -20,7 +29,6 @@ class UserDetailsViewController: UIViewController, UIScrollViewDelegate {
     
     let imageView: UIImageView = {
         let imageView = UIImageView(image: #imageLiteral(resourceName: "kelly3"))
-        imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         return imageView
@@ -28,19 +36,56 @@ class UserDetailsViewController: UIViewController, UIScrollViewDelegate {
     
     let infoLabel: UILabel = {
         let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "User name 30\nDoctor\nSome bio text down below"
         label.numberOfLines = 0
         return label
     }()
+    
+    lazy var dismissButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(Buttons.dismissDown, for: .normal)
+        button.addTarget(self, action: #selector(handleTapDismiss), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var dislikeButton = self.createButton(image: Buttons.dismiss!, selector: #selector(handleDislike))
+    lazy var superLikeButton = self.createButton(image: Buttons.superLike!, selector: #selector(handleDislike))
+    lazy var likeButton = self.createButton(image: Buttons.like!, selector: #selector(handleDislike))
+    
+    @objc private func handleDislike() {
+        print("debugging code")
+    }
+    
+    private func createButton(image: UIImage, selector: Selector) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setImage(image.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: selector, for: .touchUpInside)
+        button.imageView?.contentMode = .scaleAspectFill
+        return button
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .white
         setupLayout()
+        setupVisualBlurEffect()
+        setupBottomControls()
+    }
     
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
+    private func setupBottomControls() {
+        let stackView = UIStackView(arrangedSubviews: [dislikeButton, superLikeButton, likeButton])
+        stackView.distribution = .fillEqually
+        stackView.spacing = -32
+        view.addSubview(stackView)
+        stackView.anchor(top: nil, leading: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: nil, padding: .init(top: 0, left: 0, bottom: 0, right: 0), size: .init(width: 300, height: 80))
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    private func setupVisualBlurEffect() {
+        let blurEffect = UIBlurEffect(style: .regular)
+        let visualEffectView = UIVisualEffectView(effect: blurEffect)
+        view.addSubview(visualEffectView)
+        visualEffectView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.topAnchor, trailing: view.trailingAnchor)
     }
     
     private func setupLayout() {
@@ -54,6 +99,9 @@ class UserDetailsViewController: UIViewController, UIScrollViewDelegate {
         
         scrollView.addSubview(infoLabel)
         infoLabel.anchor(top: imageView.bottomAnchor, leading: scrollView.leadingAnchor, bottom: nil, trailing: scrollView.trailingAnchor, padding: .init(top: 16, left: 16, bottom: 0, right: 16))
+        
+        scrollView.addSubview(dismissButton)
+        dismissButton.anchor(top: imageView.bottomAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: -25, left: 0, bottom: 0, right: 24), size: .init(width: 50, height: 50))
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
