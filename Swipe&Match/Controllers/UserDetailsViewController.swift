4459a311ef999356b2c31e94d65405de836b6819
cagatay <cagatay.gedik@icloud.com>
2023-04-13 18:00:49 +0300

    Started making UserDetailsViewController.

--- Swipe&Match/Controllers/UserDetailsViewController.swift
+++ Swipe&Match/Controllers/UserDetailsViewController.swift
@@ -7,11 +7,63 @@
 
 import UIKit
 
-class UserDetailsViewController: UIViewController {
+class UserDetailsViewController: UIViewController, UIScrollViewDelegate {
+    
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.alwaysBounceVertical = true
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.delegate = self
+        return scrollView
+    }()
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "kelly3"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    let infoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "User name 30\nDoctor\nSome bio text down below"
+        label.numberOfLines = 0
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        setupLayout()
+    
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
+    }
+    
+    private func setupLayout() {
+        view.backgroundColor = .white
+        
+        view.addSubview(scrollView)
+        scrollView.fillSuperview()
+        
+        scrollView.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width)
+        
+        scrollView.addSubview(infoLabel)
+        infoLabel.anchor(top: imageView.bottomAnchor, leading: scrollView.leadingAnchor, bottom: nil, trailing: scrollView.trailingAnchor, padding: .init(top: 16, left: 16, bottom: 0, right: 16))
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let changeY = -scrollView.contentOffset.y
+        var width = view.frame.width + changeY * 2
+        width = max(view.frame.width, width)
+        imageView.frame = CGRect(x: min(0, -changeY), y: min(0, -changeY), width: width, height: width)
+    }
+    
+    @objc fileprivate func handleTapDismiss() {
+        self.dismiss(animated: true)
     }
 }
