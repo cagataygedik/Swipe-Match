5bc5fd21994ea0c874adc2ffa8d1c75173756300
cagatay <cagatay.gedik@icloud.com>
2023-04-13 16:28:35 +0300

    Created UserDetailsViewController.

---
+++ Swipe&Match/Controllers/UserDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserDetailsViewController.swift
+//  Swipe&Match
+//
+//  Created by Celil Çağatay Gedik on 13.04.2023.
+//
+
+import UIKit
+
+class UserDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
